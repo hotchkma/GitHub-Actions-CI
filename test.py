c0faf9d9998ce88f71d491cda2b3ae94c67a65c4
Matthew Hotchkiss <hotchkma@oregonstate.edu>
2021-06-03 12:34:47 -0700

    added multiply test

--- test.py
+++ test.py
@@ -9,6 +9,9 @@ class TestCase(unittest.TestCase):
     def test_subtract_1(self):
         self.assertEqual(example.subtract(2, 1), 1)
 
+    def test_multiply_1(self):
+        self.assertEqual(example.multiply(2, 3) 6)
+
 
 if __name__ == '__main__':
     unittest.main()
