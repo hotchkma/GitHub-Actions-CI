08f09ebf733f2ac12aa53758addbe1be6aefb654
Matthew Hotchkiss <hotchkma@oregonstate.edu>
2021-06-03 12:37:46 -0700

    Added divide test

--- test.py
+++ test.py
@@ -12,6 +12,9 @@ class TestCase(unittest.TestCase):
     def test_multiply_1(self):
         self.assertEqual(example.multiply(2, 3), 6)
 
+    def test_divide_1(self):
+        self.assertEqual(example.divide(6, 2), 3)
+
 
 if __name__ == '__main__':
     unittest.main()
